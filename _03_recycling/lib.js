4473a379cc48ff5b9a7ab6db2e89dc45ce87ae74
Ian Marshall <ian.js.tutor@gmail.com>
2024-02-04 09:47:39 -0500

    started lesson 3.1

---
+++ _03_recycling/lib.js
@@ -0,0 +1,29 @@
+export const PI = Math.PI;
+export const TWO_PI = Math.PI * 2;
+export const HALF_PI = Math.PI * 0.5;
+
+export function lerp(a, b, t) {
+    return a + (b - a) * t;
+}
+
+export function degToRad(d) {
+    return d * (Math.PI / 180);
+}
+
+export function radToDeg(r) {
+    return r * (180 / Math.PI);
+}
+
+export function polarToCartesian({ a, v }) {
+    return {
+        x: v * Math.cos(a),
+        y: v * Math.sin(a)
+    };
+}
+
+export function cartesianToPolar({ x, y }) {
+    return {
+        a: Math.atan2(y, x),
+        v: Math.hypot(x, y)
+    };
+}
---
+++ _03_recycling/particles.js
@@ -0,0 +1,90 @@
+//dependencies
+import { lerp, TWO_PI, polarToCartesian } from "./lib.js";
+
+//settings
+const numParticles = 250;
+const minRadius = 2;
+const maxRadius = 5;
+const minSpeed = 2;
+const maxSpeed = 5;
+const acceleration = 0.98;
+const opacity = 1;
+const minLife = 75;
+const maxLife = 125;
+const color = "white";
+const respawn = true;
+
+//state
+const particles = [];
+const emitter = { x: undefined, y: undefined };
+
+//setup
+function setupParticles() {
+    for (let i=0; i<numParticles; i++) {
+        particles[i] = getParticle();
+    }
+}
+
+function getParticle() {
+    const angle = lerp(0, TWO_PI, Math.random());
+    const speed = lerp(minSpeed, maxSpeed, Math.random());
+    const { x: vx, y: vy } = polarToCartesian({ a: angle, v: speed });
+    const { x, y } = emitter;
+    const r = lerp(minRadius, maxRadius, Math.random());
+    const life = Math.round(lerp(minLife, maxLife, Math.random()));
+    return { x, y, vx, vy, r, opacity, color, life };
+}
+
+function resetParticle(p) {
+    const { x, y, vx, vy, r, opacity, color, life } = getParticle();
+    p.x = x;
+    p.y = y;
+    p.vx = vx;
+    p.vy = vy;
+    p.r = r;
+    p.opacity = opacity;
+    p.color = color;
+    p.life = life;
+}
+
+export function setEmitter({ x, y }) {
+    emitter.x = x;
+    emitter.y = y;
+    setupParticles();
+}
+
+//loop functions
+export function update({ width, height }) {
+    //update particles
+    // for (let i=0; i<particles.length; i++) {
+    //     let p = particles[i];
+    for (let p of particles) {
+        //needs respawning?
+        if (p.life <= 0) {
+            if (respawn) resetParticle(p); //particles[i] = getParticle();
+            continue;
+        }
+        //move and accelerate, change opacity, life
+        p.x += p.vx;
+        p.y += p.vy;
+        p.vx *= acceleration;
+        p.vy *= acceleration;
+        p.opacity *= acceleration;
+        p.life--;
+    }
+}
+
+export function draw(ctx) {
+    ctx.save();
+    for (let { x, y, r, opacity, color, life } of particles) {
+        //is no longer live?
+        if (life <= 0) continue;
+        //it's alive!
+        ctx.globalAlpha = opacity;
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, TWO_PI);
+        ctx.fill();
+    }
+    ctx.restore();
+}
